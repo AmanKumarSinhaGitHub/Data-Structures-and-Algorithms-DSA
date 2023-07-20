aff1f4a08773e288575404ef3409726c852f762e
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-07-20 21:07:01 +0530

    Initial Commit

---
+++ 00 - STL/12 - Multi Map/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainMultimap() {
+
+	// everything same as map, 
+	// but it can store multiple or duplicate keys
+
+	// mpp[key] cannot be used here 
+	
+}
+
+int main(){
+
+	explainMultimap();
+	
+    return 0;
+}
---
+++ 00 - STL/13 - Unordered Map/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void explainUnorderedMap() {
+	
+    // store unique keys
+
+    // it does not store elements in a sorted order.
+    // Elements are stored in a random order
+
+    // Time complexity : O(1) almost everytime
+}
+
+
+int main(){
+
+    explainUnorderedMap();
+
+    return 0;
+}
